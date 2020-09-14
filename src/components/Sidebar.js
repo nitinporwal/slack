a6fd194f88fb8c8cf3499b0498e9d43bdfcb151f
nitin-codr <nitinporwal99@gmail.com>
2020-09-14 22:16:48 +0530

    add sidebar componets

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 export default class Sidebar extends Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div>
+        <div className='user-profile'>
+          <div>
+            <img />
+          </div>
+          <div>Ronald</div>
+        </div>
+        <div className='user-channel'></div>
+      </div>
+    );
   }
 }
